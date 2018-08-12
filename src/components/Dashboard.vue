3894bf21153bf0adcd84e00a95955582d75d4df1
Maks <maks_zhukov_97@mail.ru>
2018-08-12 20:17:23 +0300

    adde delete button and selected

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="dashboard">
 		<div class="container">
-		<div class="file-field input-field">
-      <div class="btn-floating waves-effect waves-light red">
-        <i class="material-icons">add</i>
-        <input type="file" multiple @change="changeAdd" id="file" accept=".jpg, .jpeg, .png">
-      </div>
-    </div>
+			<div class="row">
+				<div class="col s12">
+					<button class="btn-floating waves-effect file-field input-field waves-light red btn-add-image">
+						<i class="material-icons">add</i>
+						<input type="file" multiple @change="changeAdd" id="file" accept=".jpg, .jpeg, .png">
+					</button>
+					<button class="btn-floating waves-effect waves-light red" @click="clickRemove">
+						<i class="material-icons">remove</i>
+					</button>
+					<button v-if="isBtnRemoveActive" :class="['btn',images.some(imageInfo => imageInfo.selected === true ) ? '' : 'disabled']" @click="clickDelete">delete</button>
+				</div>
+		</div>
 			<transition-group name="list" tag="div" class="list-images">
-			<div class="list-images-item" v-for="image in images" :key="image.key">
+			<div :class="[image.selected ? 'list-images-item-selected' : '', 'list-images-item']" v-for="image in images" :key="image.key">
 				<img :src="image.url" :alt="image.name" :data-key="image.key" class="responsive-img" @click="clickImg">
 			</div>
 			</transition-group>
@@ -22,6 +28,7 @@ export default {
   name: 'Dashboard',
   data () {
     return {
+			isBtnRemoveActive: false
     }
 	},
 	created(){
@@ -37,7 +44,10 @@ export default {
 		...mapActions('imagesModule',[
 			'getImages',
       'addImage',
-      'deleteImage'
+			'deleteImages'
+		]),
+		...mapMutations('imagesModule',[
+			'selectImage'
 		]),
 		changeAdd({target}){
       for (let file of target.files){
@@ -45,9 +55,19 @@ export default {
       }
     },
     clickImg({target}){
-      const key = +target.dataset.key
-      this.deleteImage(key)
-    }
+			if (this.isBtnRemoveActive){
+			const key = +target.dataset.key
+			this.selectImage(key)
+			}
+		},
+		clickRemove(event){
+			this.isBtnRemoveActive = !this.isBtnRemoveActive
+		},
+		clickDelete(event){
+			if (this.isBtnRemoveActive){
+				this.deleteImages()
+			}
+		}
 	}
 }
 </script>
@@ -58,12 +78,19 @@ export default {
 	width: 100%
 	min-height: 300px
 	padding: 20px 0 
+.btn-add-image
+	margin: 0 10px 0 0
 .list-images
 	display: grid
 	grid-gap: 10px
-	align-items: center
 	grid-template-columns: repeat(6,1fr)
 	grid-auto-rows: 150px
+	&-item
+		height: 100%
+		display: flex
+		align-items: center
+		&-selected
+			border: 2px solid red
 .list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */
   opacity: 0
   transform: translateY(30px)
--- src/store/modules/images/index.js
+++ src/store/modules/images/index.js
@@ -17,193 +17,311 @@ const userModule = {
   namespaced: true,
   state: getDefaultState(),
   mutations: {
-    updateNumberInsertNext (state, newNumberInsertNext) {
+    updateNumberInsertNext(state, newNumberInsertNext) {
       state.numberInsertNext = newNumberInsertNext
     },
-    addImagePending (state, responseAddImage) {
+    addImagePending(state, responseAddImage) {
       state.responseAddImage = responseAddImage
     },
-    addImageSuccess (state, {response, imageInfo}) {
+    addImageSuccess(state, { response, imageInfo }) {
       state.images.push(imageInfo)
       state.responseAddImage = response
     },
-    addImageError (state, responseAddImage) {
+    addImageError(state, responseAddImage) {
       state.responseAddImage = responseAddImage
     },
-    addImageToStoragePending (state, responseAddImageToStorage) {
+    addImageToStoragePending(state, responseAddImageToStorage) {
       state.responseAddImageToStorage = responseAddImageToStorage
     },
-    addImageToStorageSuccess (state, responseAddImageToStorage) {
+    addImageToStorageSuccess(state, responseAddImageToStorage) {
       state.responseAddImageToStorage = responseAddImageToStorage
     },
-    addImageToStorageError (state, responseAddImageToStorage) {
+    addImageToStorageError(state, responseAddImageToStorage) {
       state.responseAddImageToStorage = responseAddImageToStorage
     },
-    addImageToDBPending (state, responseAddImageToDB) {
+    addImageToDBPending(state, responseAddImageToDB) {
       state.responseAddImageToDB = responseAddImageToDB
     },
-    addImageToDBSuccess (state, responseAddImageToDB) {
+    addImageToDBSuccess(state, responseAddImageToDB) {
       state.responseAddImageToDB = responseAddImageToDB
     },
-    addImageToDBError (state, responseAddImageToDB) {
+    addImageToDBError(state, responseAddImageToDB) {
       state.responseAddImageToDB = responseAddImageToDB
     },
-    getImagePending (state, responseGetImage) {
+    getImagePending(state, responseGetImage) {
       state.responseGetImage = responseGetImage
     },
-    getImageSuccess (state, responseGetImage) {
+    getImageSuccess(state, responseGetImage) {
       state.responseGetImage = responseGetImage
     },
-    getImageError (state, responseGetImage) {
+    getImageError(state, responseGetImage) {
       state.responseGetImage = responseGetImage
     },
-    getImagesPending (state, responseGetImages) {
+    getImagesPending(state, responseGetImages) {
       state.responseGetImages = responseGetImages
     },
-    getImagesSuccess (state, responseGetImages) {
+    getImagesSuccess(state, responseGetImages) {
       state.responseGetImages = responseGetImages
     },
-    getImagesError (state, responseGetImages) {
+    getImagesError(state, responseGetImages) {
       state.responseGetImages = responseGetImages
     },
-    deleteImagePending (state, responseDeleteImage) {
+    deleteImagePending(state, responseDeleteImage) {
       state.responseDeleteImage = responseDeleteImage
     },
-    deleteImageSuccess (state, {response, key}) {
-      const image = state.images.find(image => image.key === key)
-      const index = state.images.indexOf(image)
-      state.images.splice(index, 1)
+    deleteImageSuccess(state, response) {
+      state.images.forEach((imageInfo,index)=>{
+				if (imageInfo.selected){
+					state.images.splice(index,1)
+				}
+			})
       state.responseDeleteImage = response
     },
-    deleteImageError (state, responseDeleteImage) {
+    deleteImageError(state, responseDeleteImage) {
       state.responseDeleteImage = responseDeleteImage
     },
-    deleteImageFromDBPending (state, responseDeleteImageDB) {
+    deleteImageFromDBPending(state, responseDeleteImageDB) {
       state.responseDeleteImageDB = responseDeleteImageDB
     },
-    deleteImageFromDBSuccess (state, responseDeleteImageDB) {
+    deleteImageFromDBSuccess(state, responseDeleteImageDB) {
       state.responseDeleteImageDB = responseDeleteImageDB
     },
-    deleteImageFromDBError (state, responseDeleteImageDB) {
+    deleteImageFromDBError(state, responseDeleteImageDB) {
       state.responseDeleteImageDB = responseDeleteImageDB
     },
-    deleteImageFromStoragePending (state, responseDeleteImageFromStorage) {
+    deleteImageFromStoragePending(state, responseDeleteImageFromStorage) {
       state.responseDeleteImageFromStorage = responseDeleteImageFromStorage
     },
-    deleteImageFromStorageSuccess (state, responseDeleteImageFromStorage) {
+    deleteImageFromStorageSuccess(state, responseDeleteImageFromStorage) {
       state.responseDeleteImageFromStorage = responseDeleteImageFromStorage
     },
-    deleteImageFromStorageError (state, responseDeleteImageFromStorage) {
+    deleteImageFromStorageError(state, responseDeleteImageFromStorage) {
       state.responseDeleteImageFromStorage = responseDeleteImageFromStorage
     },
-    resetImages (state) {
+    resetImages(state) {
       const defaultState = getDefaultState()
       Object.keys(defaultState).forEach(key => {
         state[key] = defaultState[key]
       })
+    },
+    selectImage(state, key) {
+      const image = state.images.find(image => image.key === key)
+      image.selected = !image.selected
     }
   },
   actions: {
-    getImages ({commit, dispatch}) {
-      commit('getImagesPending', {pending: true})
+    getImages({ commit, dispatch }) {
+      commit('getImagesPending', { pending: true })
       const id = this.state.userModule.user.uid
-      fs.collection('images').doc(id).get().then(function (images) {
-        if (images.exists) {
-          commit('getImagesSuccess', {pending: false, status: 'success', message: 'images was found'})
-          const data = images.data()
-          for (let key in data) {
-            commit('addImageSuccess', { response: {pending: false, status: 'success', message: 'add image success'}, imageInfo: { key: +key, name: data[key].name, url: data[key].url } })
-            commit('updateNumberInsertNext', +key)
+      fs.collection('images')
+        .doc(id)
+        .get()
+        .then(function(images) {
+          if (images.exists) {
+            commit('getImagesSuccess', {
+              pending: false,
+              status: 'success',
+              message: 'images was found'
+            })
+            const data = images.data()
+            for (let key in data) {
+              commit('addImageSuccess', {
+                response: {
+                  pending: false,
+                  status: 'success',
+                  message: 'add image success'
+                },
+                imageInfo: {
+                  key: +key,
+                  name: data[key].name,
+                  url: data[key].url,
+                  selected: false
+                }
+              })
+              commit('updateNumberInsertNext', +key)
+            }
+          } else {
+            commit('getImagesError', {
+              pending: false,
+              status: 'error',
+              message: 'No such document'
+            })
           }
-        } else {
-          commit('getImagesError', {pending: false, status: 'error', message: 'No such document'})
-        }
-      }).catch(function (error) {
-        commit('getImagesError', {pending: false, status: 'error', message: error})
-      })
+        })
+        .catch(function(error) {
+          commit('getImagesError', {
+            pending: false,
+            status: 'error',
+            message: error
+          })
+        })
     },
-    async addImage ({commit, state, dispatch}, file) {
+    async addImage({ commit, state, dispatch }, file) {
       try {
         const id = this.state.userModule.user.uid
         const key = state.numberInsertNext + 1
         commit('updateNumberInsertNext', key)
         const { name } = file
         commit('addImagePending', { pending: true })
-        await dispatch('addImageToStorage', {id, key, name, file})
-        const url = await dispatch('getImage', {id, key, name})
-        await dispatch('addImageToDB', {id, key, name, url})
-        commit('addImageSuccess', { response: {pending: false, status: 'success', message: 'add image success'}, imageInfo: {key, name, url} })
+        await dispatch('addImageToStorage', { id, key, name, file })
+        const url = await dispatch('getImage', { id, key, name })
+        await dispatch('addImageToDB', { id, key, name, url })
+        commit('addImageSuccess', {
+          response: {
+            pending: false,
+            status: 'success',
+            message: 'add image success'
+          },
+          imageInfo: { key, name, url, selected: false }
+        })
       } catch (error) {
         commit('addImageError', { error, pending: false })
       }
     },
-    async getImage ({commit}, {id, key, name}) {
+    async getImage({ commit }, { id, key, name }) {
       try {
         let url
         commit('getImagePending', { pending: false })
-        await storage.ref(`images/${id}/${key + name}`).getDownloadURL()
-          .then((urlResponse) => {
-            commit('getImageSuccess', { status: 'success', pending: false, message: 'image get success' })
+        await storage
+          .ref(`images/${id}/${key + name}`)
+          .getDownloadURL()
+          .then(urlResponse => {
+            commit('getImageSuccess', {
+              status: 'success',
+              pending: false,
+              message: 'image get success'
+            })
             url = urlResponse
           })
         return url
       } catch (error) {
-        commit('getImageError', { pending: false, status: 'error', message: error })
+        commit('getImageError', {
+          pending: false,
+          status: 'error',
+          message: error
+        })
       }
     },
-    addImageToDB ({commit}, {id, key, name, url}) {
+    addImageToDB({ commit }, { id, key, name, url }) {
       commit('addImageToDBPending', { pending: true })
-      return fs.collection('images').doc(id).set({[key]: {name, url}}, { merge: true })
+      return fs
+        .collection('images')
+        .doc(id)
+        .set({ [key]: { name, url } }, { merge: true })
         .then(() => {
-          commit('addImageToDBSuccess', {status: 'success', message: 'added to database'})
+          commit('addImageToDBSuccess', {
+            status: 'success',
+            message: 'added to database'
+          })
         })
-        .catch(function (error) {
-          commit('addImageError', { status: 'error', pending: false, message: error })
+        .catch(function(error) {
+          commit('addImageError', {
+            status: 'error',
+            pending: false,
+            message: error
+          })
         })
     },
-    addImageToStorage ({commit, dispatch}, {id, key, name, file}) {
+    addImageToStorage({ commit, dispatch }, { id, key, name, file }) {
       try {
         commit('addImageToStoragePending', { pending: true })
-        return storage.ref(`images/${id}/${key + name}`).put(file).then((doc) => {
-          commit('addImageToStorageSuccess', { pending: false, status: 'success', message: 'add to storage success' })
-          dispatch('getImage', { id, key, name })
-        })
+        return storage
+          .ref(`images/${id}/${key + name}`)
+          .put(file)
+          .then(doc => {
+            commit('addImageToStorageSuccess', {
+              pending: false,
+              status: 'success',
+              message: 'add to storage success'
+            })
+            dispatch('getImage', { id, key, name })
+          })
       } catch (error) {
-        commit('addImageToStorageError', { pending: true, status: 'error', message: error })
+        commit('addImageToStorageError', {
+          pending: true,
+          status: 'error',
+          message: error
+        })
       }
     },
-    deleteFromDB ({commit}, {key, id}) {
+    deleteFromDB({ commit, state }, id) {
       commit('deleteImageFromDBPending', { pending: true })
-      return fs.collection('images').doc(id).update({
-        [key]: firebase.firestore.FieldValue.delete()
-      })
-        .then((doc) => {
-          commit('deleteImageFromDBSuccess', { pending: false, status: 'success', message: 'image was deleted from DB' })
+      const update = {}
+      for (let imageInfo of state.images) {
+        if (imageInfo.selected) {
+          update[imageInfo.key] = firebase.firestore.FieldValue.delete()
+        }
+			}
+      return fs
+        .collection('images')
+        .doc(id)
+        .update(update)
+        .then(doc => {
+          commit('deleteImageFromDBSuccess', {
+            pending: false,
+            status: 'success',
+            message: 'image was deleted from DB'
+          })
         })
-        .catch((error) => {
-          commit('deleteImageFromDBError', { pending: false, status: 'error', message: error })
+        .catch(error => {
+					console.log(error)
+          commit('deleteImageFromDBError', {
+            pending: false,
+            status: 'error',
+            message: error
+          })
         })
     },
-    deleteFromStorage ({commit}, {id, key, name}) {
+    deleteFromStorage({ commit, state }, id) {
       commit('deleteImageFromStoragePending', { pending: true })
-      return storage.ref(`images/${id}/${key + name}`).delete()
-        .then((doc) => {
-          commit('deleteImageFromStorageSuccess', { pending: false, status: 'success', message: 'image was deleted from storage' })
+      return Promise.all(
+        state.images.map(async imageInfo => {
+          if (imageInfo.selected) {
+						const name = imageInfo.key + imageInfo.name
+            await storage
+              .ref(`images/${id}/${name}`)
+              .delete()
+              .then(() => {
+                commit('deleteImageFromStorageSuccess', {
+                  pending: false,
+                  status: 'success',
+                  message: `image ${name} was deleted from storage`
+                })
+              })
+              .catch(error => {
+                commit('deleteImageFromStorageError', {
+                  pending: false,
+                  status: 'error',
+                  message: name + error.message
+                })
+              })
+          }
         })
-        .catch((error) => {
-          commit('deleteImageFromStorageError', { pending: false, status: 'error', message: error })
-        })
+      )
+        .then((doc)=>{
+					console.log(doc)
+				})
+        .catch((error)=>{
+					console.log(error)
+				})
     },
-    async deleteImage ({commit, dispatch, state}, key) {
+    async deleteImages({ commit, dispatch }) {
       try {
         commit('deleteImagePending', { pending: true })
         const id = this.state.userModule.user.uid
-        const image = state.images.find(image => image.key === key)
-        await dispatch('deleteFromDB', {id, key})
-        await dispatch('deleteFromStorage', { id, key, name: image.name })
-        commit('deleteImageSuccess', {response: {pending: false, status: 'success', message: 'error' }, key})
+        await dispatch('deleteFromDB', id)
+        await dispatch('deleteFromStorage', id)
+        commit('deleteImageSuccess', {
+          pending: false,
+          status: 'success',
+          message: 'images was deleted'
+        })
       } catch (error) {
-        commit('deleteImageError', { pending: false, status: 'error', message: 'error' })
+        commit('deleteImageError', {
+          pending: false,
+          status: 'error',
+          message: 'error'
+        })
       }
     }
   }
